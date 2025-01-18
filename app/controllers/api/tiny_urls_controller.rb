c6d96904591c168a064d4901685066197e4e2c30
samikshaglv <samikshaglv@gmail.com>
2025-01-18 16:54:58 +0530

    Added show action to redirect you your link path

--- app/controllers/api/tiny_urls_controller.rb
+++ app/controllers/api/tiny_urls_controller.rb
@@ -10,6 +10,17 @@ class Api::TinyUrlsController < ApplicationController
     end
   end
 
+# GET /:short_token
+  def show
+    tiny_url = TinyUrl.find_by(short_token: params[:short_token])
+
+    if tiny_url
+      redirect_to tiny_url.long_url, allow_other_host: true
+    else
+      render json: { error: 'URL not found' }, status: :gone
+    end
+  end
+
   private
 
   # Strong params
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   namespace :api do
     resources :tiny_urls, only: [:create]
   end
+
+  # Route for accessing short URLs
+  get '/:short_token', to: 'api/tiny_urls#show', as: :short_url
+  
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
